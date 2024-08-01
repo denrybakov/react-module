c529509f1d4a72e53828d20b31575f174827003f
denrybakov <rybakovdev@yandex.ru>
2024-08-01 23:03:12 +0300

    XO

--- src/hooks/useGameLogic.js
+++ src/hooks/useGameLogic.js
@@ -3,29 +3,23 @@ import { useEffect } from "react";
 import { findChampion, findIndexElem } from "../utils/Utils";
 
 export const useGameLogic = (state, setState) => {
-
   useEffect(() => {
     const isChampX = findChampion(findIndexElem('X', state.field));
     const isChampO = findChampion(findIndexElem('O', state.field));
     const emptyField = state.field.filter(el => el === '');
 
     if (isChampX) {
-      setState(prevState => ({
-        ...prevState, currentPlayer: 'X', isGameEnded: true
-      }));
+      setState(prevState => ({ ...prevState, currentPlayer: 'X', isGameEnded: true }));
     }
 
     if (isChampO) {
-      setState(prevState => ({
-        ...prevState, currentPlayer: 'O', isGameEnded: true
-      }));
+      setState(prevState => ({ ...prevState, currentPlayer: 'O', isGameEnded: true }));
     }
 
-    if (emptyField.length === 0) {
-      setState(prevState => ({
-        ...prevState, isDraw: true, isGameEnded: true
-      }));
+    if (emptyField.length === 0 && !isChampO && !isChampX) {
+      setState(prevState => ({ ...prevState, isDraw: true, isGameEnded: true }));
     }
+
   }, [state.field, setState]);
 }
 
